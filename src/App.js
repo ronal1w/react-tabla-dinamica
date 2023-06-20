1a8b696e2469c9b6ec539a9bda061b40295470d2
Herrera <guylatine@gmil.com>
2023-06-20 13:24:47 -0600

    installandp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TableRenderers from '../src/TableRenderers';
+import createPlotlyComponent from 'react-plotly.js/factory';
+import createPlotlyRenderers from '../src/PlotlyRenderers';
+import PivotTableUI from '../src/PivotTableUI';
+import '../src/pivottable.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const Plot = createPlotlyComponent(window.Plotly);
+
+class PivotTableUISmartWrapper extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { pivotState: props };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ pivotState: nextProps });
+  }
+
+  render() {
+    return (
+      <PivotTableUI
+        renderers={Object.assign(
+          {},
+          TableRenderers,
+          createPlotlyRenderers(Plot)
+        )}
+        {...this.state.pivotState}
+        onChange={(s) => this.setState({ pivotState: s })}
+        unusedOrientationCutoff={Infinity}
+      />
+    );
+  }
 }
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'api',
+      filename: 'Data from API',
+      pivotState: { data: [] },
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDataFromAPI();
+  }
+
+  fetchDataFromAPI() {
+    const apiUrl = 'http://127.0.0.1:8000/api/ventas';
+  
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        if (response.status === 200) {
+          const data = response.data; // Obtenemos los datos del arreglo de objetos directamente
+  
+          this.setState({
+            mode: 'api',
+            filename: 'Data from API',
+            pivotState: { data },
+          });
+          
+        } else {
+          console.error('Error fetching data from API. Response status:', response.status);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data from API:', error);
+      });
+  }
+  
+  
+  
+
+  render() {
+    return (
+      <div>
+        <div className="row text-center">
+          <div className="col-md-3 col-md-offset-3"></div>
+        </div>
+        <div className="row">
+          <h2 className="text-center">{this.state.filename}</h2>
+          <br />
+
+          {this.state.mode === 'api' ? (
+            <PivotTableUISmartWrapper {...this.state.pivotState} />
+          ) : (
+            <p>Loading data from API...</p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
