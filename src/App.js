757f9069402b517dc48315ce56e50324f1a1cfa4
Herrera <guylatine@gmil.com>
2023-06-20 14:26:58 -0600

    funciona

--- src/App.js
+++ src/App.js
@@ -1,99 +1,79 @@
-import React from 'react';
-import TableRenderers from '../src/TableRenderers';
-import createPlotlyComponent from 'react-plotly.js/factory';
-import createPlotlyRenderers from '../src/PlotlyRenderers';
-import PivotTableUI from '../src/PivotTableUI';
-import '../src/pivottable.css';
+import React, { useEffect, useState } from 'react';
+import PivotTableUI from 'react-pivottable';
+import 'react-pivottable/pivottable.css';
 import axios from 'axios';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
+const PivotTableUISmartWrapper = () => {
+  const [data, setData] = useState([]);
+  const [pivotState, setPivotState] = useState({ data, pivotState: {} });
 
-
-const Plot = createPlotlyComponent(window.Plotly);
-
-class PivotTableUISmartWrapper extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { pivotState: props };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({ pivotState: nextProps });
-  }
-
-  render() {
-    return (
-      <PivotTableUI
-        renderers={Object.assign(
-          {},
-          TableRenderers,
-          createPlotlyRenderers(Plot)
-        )}
-        {...this.state.pivotState}
-        onChange={(s) => this.setState({ pivotState: s })}
-        unusedOrientationCutoff={Infinity}
-      />
-    );
-  }
-}
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: 'api',
-      filename: 'Data from API',
-      pivotState: { data: [] },
-    };
-  }
-
-  componentDidMount() {
-    this.fetchDataFromAPI();
-  }
-
-  fetchDataFromAPI() {
+  const fetchDataFromAPI = () => {
     const apiUrl = 'http://127.0.0.1:8000/api/ventas';
-  
+
     axios
       .get(apiUrl)
-      .then((response) => {
+      .then(response => {
         if (response.status === 200) {
-          const data = response.data; // Obtenemos los datos del arreglo de objetos directamente
-  
-          this.setState({
-            mode: 'api',
-            filename: 'Data from API',
-            pivotState: { data },
-          });
-          
+          const data = response.data;
+          setData(data);
         } else {
-          console.error('Error fetching data from API. Response status:', response.status);
+          console.error('Error al obtener datos desde la API. Estado de respuesta:', response.status);
+          alert('Error al obtener datos desde la API. Por favor, intenta nuevamente más tarde.');
         }
       })
-      .catch((error) => {
-        console.error('Error fetching data from API:', error);
+      .catch(error => {
+        console.error('Error al obtener datos desde la API:', error);
+        alert('Error al obtener datos desde la API. Por favor, intenta nuevamente más tarde.');
       });
-  }
-  
-  
-  
+  };
 
-  render() {
-    return (
+  const exportToExcel = () => {
+    const table = document.querySelector('.pvtTable');
+    if (!table) {
+      console.warn('No se encontró la tabla.');
+      return;
+    }
+
+    const worksheet = XLSX.utils.table_to_sheet(table);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    const data = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    saveAs(data, 'pivot_table_data.xlsx');
+  };
+
+  useEffect(() => {
+    fetchDataFromAPI();
+  }, []);
+
+  useEffect(() => {
+    setPivotState(prevState => ({ ...prevState, data }));
+  }, [data]);
+
+  return (
+    <div>
       <div>
-        <div className="row text-center">
-          <div className="col-md-3 col-md-offset-3"></div>
-        </div>
-        <div className="row">
-          <h2 className="text-center">{this.state.filename}</h2>
-          <br />
-
-          {this.state.mode === 'api' ? (
-            <PivotTableUISmartWrapper {...this.state.pivotState} />
-          ) : (
-            <p>Loading data from API...</p>
-          )}
-        </div>
+        <button onClick={exportToExcel}>Exportar a Excel</button>
       </div>
-    );
-  }
-}
+      <PivotTableUI
+        {...pivotState}
+        onChange={s => {
+          setPivotState(s);
+          //console.log(s.data);
+        }}
+        unusedOrientationCutoff={Infinity}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  return <PivotTableUISmartWrapper />;
+};
+
+export default App;
